e350185290f4b2415d2c1b08be45c992fc1dcf44
unknown <larryfemi8@gmail.com>
2022-04-07 11:49:50 +0100

    Added Ejs Rendering

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 require("dotenv").config();
 
 const port = 5050;
 const AppKey = process.env.API_KEY;
 
+app.set("view engine", "ejs");
+app.use("/public", express.static("public"));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const appData = {
 	appkey: AppKey,
 };
+
+app.get("/", (req, res) => {
+	res.render("index");
+});
 
 app.post("/weather", (req, res) => {
 	console.log(req.body);
