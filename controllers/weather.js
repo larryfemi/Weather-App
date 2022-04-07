9aa15e68a1d43edd600f90f4eb0800d4bb19b082
oluwafemi <larryfemi8@gmail.com>
2022-04-07 21:53:28 +0100

    changes made

---
+++ controllers/weather.js
@@ -0,0 +1,24 @@
+const fetch = require("node-fetch");
+const { response } = require("express");
+require("dotenv").config();
+
+const appKey = process.env.API_KEY;
+
+const weatherData = async () => {
+	const city = "london";
+	const url = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${appKey}`;
+	const getData = await fetch(url);
+	const response = await getData.json();
+	const lat = response[0].lat;
+	const lon = response[0].lon;
+	const newUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appKey}`;
+	try {
+		const newData = await fetch(newUrl);
+		const data = await newData.json();
+		console.log(data);
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+module.exports = { weatherData };
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const bodyParser = require("body-parser");
 const fetch = require("node-fetch");
 const { response } = require("express");
+const { weatherData } = require("./controllers/weather");
+const e = require("express");
 require("dotenv").config();
 
 const port = 5050;
@@ -14,25 +16,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(bodyParser.json());
 
-const city = "london";
-const url = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${appKey}`;
-
-async function newData(url) {
-	const getData = await fetch(url)
-		.then((res) => res.json())
-		.catch((e) => console.log(e));
-	const lat = getData[0].lat;
-	const lon = getData[0].lon;
-	const newUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appKey}`;
-	try {
-		const newData = await fetch(newUrl).then((res) => res.json());
-		console.log(newData);
-	} catch (error) {
-		console.log(error);
-	}
-}
-
-newData(url);
+weatherData();
 
 app.get("/", async (req, res) => {
 	res.render("index");
